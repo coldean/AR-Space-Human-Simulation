467a0946995f55573109ed94003f712d197f76db
jisung0603@gmail.com <jisung0603@gmail.com>
2024-05-14 21:35:32 +0900

    little changed GetLocation
    It will show change once at time.
    Showing plane information, position, size, rotation

--- Assets/Scripts/GetLocation.cs
+++ Assets/Scripts/GetLocation.cs
@@ -5,6 +5,7 @@ using UnityEngine.XR.ARFoundation;
 
 public class GetLocation : MonoBehaviour
 {
+    /*
     private ARPlaneManager _arPlaneManager;
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,25 @@ public class GetLocation : MonoBehaviour
             Vector3 planePosition = plane.transform.position;
             Quaternion planeRotation = plane.transform.rotation;
             Debug.Log($"Plane position: {planePosition}, Plane rotation: {planeRotation}");
+        }
+    }
+    */
+    public ARPlaneManager planeManager;
+
+    void Start()
+    {
+        planeManager = GetComponent<ARPlaneManager>();
+        planeManager.planesChanged += OnPlanesChanged;
+    }
+
+    void OnPlanesChanged(ARPlanesChangedEventArgs eventArgs)
+    {
+        foreach (var plane in eventArgs.added)
+        {
+            Debug.Log("Detected new plane: " + plane);
+            Debug.Log("Position: " + plane.transform.position);
+            Debug.Log("Size: " + plane.size);
+            Debug.Log("Rotation: " + plane.transform.rotation);
         }
     }
 }
