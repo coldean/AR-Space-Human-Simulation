24e10e2520bf3c9dd7a46729a0d69935622d8e01
jisung0603@gmail.com <jisung0603@gmail.com>
2024-11-02 00:18:18 +0900

    fixed location bug (axis y)

--- Assets/Scripts/GenerateRandomPerson.cs
+++ Assets/Scripts/GenerateRandomPerson.cs
@@ -108,14 +108,14 @@ public class RandomPersonPlacer : MonoBehaviour
             {
                 // 확률에 따라 위치 조정
                 //float distanceModifier = Mathf.Lerp(1f, 0f, locProb.probability); // 확률에 따라 위치를 멀리 또는 가깝게 생성
-                float distanceModifier = (1f - locProb.probability) / 5; // 이걸로 진행하면 될듯 /////////////////////////////////////////////////
+                float distanceModifier = (1f - locProb.probability) / 2; // 이걸로 진행하면 될듯 /////////////////////////////////////////////////
 
                 Vector3 offset = new Vector3(
                     Random.Range(-distanceModifier, distanceModifier),
                     0,
                     Random.Range(-distanceModifier, distanceModifier)
                 );
-                Vector3 tempPosition = new Vector3(locProb.location.x, locProb.location.y, -locProb.location.z);
+                Vector3 tempPosition = new Vector3(locProb.location.x, locProb.location.y, locProb.location.z);
                 //Vector3 spawnPosition = locProb.location + offset;
                 Vector3 spawnPosition = tempPosition + offset;
                 Vector3 planePosition = GetRandomPointInPlane(spawnPosition);
@@ -197,8 +197,8 @@ public class RandomPersonPlacer : MonoBehaviour
         // 상관없을 듯 하다, 사람들 count를 generate 할 때마다 새로 갱신하기 때문에 여기서 생성이 안되어 기록이 안되어도 상관없음. 대신 여기서 return값이
         // 무조건 좌표라, 무조건 생성되는데...  이 문제는 생각해 봐야 할듯. flag를 만들어서 하는게 가장 직관적이긴 함.
 
-        Vector3 randomPosition = specificLocation.Value;
-        // Vector3 randomPosition = new Vector3(specificLocation.Value.x, selectedPlane.transform.position.y, specificLocation.Value.z); // 이렇게 진행 /////////////////////
+        //Vector3 randomPosition = specificLocation.Value;
+        Vector3 randomPosition = new Vector3(specificLocation.Value.x, selectedPlane.transform.position.y, -specificLocation.Value.z); // 이렇게 진행 /////////////////////
         // 특정 위치가 주어지면 그 위치를 기반으로 평면 내 무작위 위치를 반환합니다.
         if (specificLocation.HasValue)
         {
@@ -213,8 +213,8 @@ public class RandomPersonPlacer : MonoBehaviour
         }
         //봄
         // random 이니까 count 해야 하긴 해서 position 값 줘서 만드는게 맞기는 한듯.
-        //return randomPosition;
-        return new Vector3(0, 10, 0);
+        return randomPosition;
+        //return new Vector3(0, 10, 0);
 
         // 내 생각에 밑부분은 필요하지 않은듯 ///////////////////////////
         // 선택된 평면의 경계 내에서 무작위 위치를 생성합니다.
